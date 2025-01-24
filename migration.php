9f607dbc1e3c5126c777f24072346803a2f623fe
iluxaar <15342468+iluxaar@users.noreply.github.com>
2025-01-24 16:04:56 +0500

    Update migration.php

--- migration.php
+++ migration.php
@@ -12,20 +12,20 @@ class m241029_205648_init extends Migration
      */
     public function safeUp()
     {
-		$this->createTable('users', [
-			'id' => $this->primaryKey(),
-			'email' => $this->string(255)->notNull()->unique()->comment('Email'),
-		]);
-		
-		$this->createTable('orders', [
-			'id' => $this->primaryKey(),
-			'user_id' => $this->integer()->notNull()->comment('ID пользователя'),
-			'created_at' => $this->dateTime()->notNull()->defaultExpression('CURRENT_TIMESTAMP')->comment('Время заказа'),
-			'total_amount' => $this->bigInteger()->notNull()->comment('Сумма в минимальной денежной единице')
-		]);
+	$this->createTable('users', [
+		'id' => $this->primaryKey(),
+		'email' => $this->string(255)->notNull()->unique()->comment('Email'),
+	]);
+	
+	$this->createTable('orders', [
+		'id' => $this->primaryKey(),
+		'user_id' => $this->integer()->notNull()->comment('ID пользователя'),
+		'created_at' => $this->dateTime()->notNull()->defaultExpression('CURRENT_TIMESTAMP')->comment('Время заказа'),
+		'total_amount' => $this->bigInteger()->notNull()->comment('Сумма в минимальной денежной единице')
+	]);
 	    
-	    $this->addForeignKey('users_orders_fk', 'orders', 'user_id', 'users', 'id');
-		$this->createIndex('orders_total_amount_index', 'orders', 'total_amount');
+	$this->addForeignKey('users_orders_fk', 'orders', 'user_id', 'users', 'id');
+	$this->createIndex('orders_total_amount_index', 'orders', 'total_amount');
     }
 
     /**
@@ -33,9 +33,9 @@ class m241029_205648_init extends Migration
      */
     public function safeDown()
     {
-	    $this->dropForeignKey('users_orders_fk', 'orders');
+	$this->dropForeignKey('users_orders_fk', 'orders');
         $this->dropTable('orders');
-	    $this->dropTable('users');
+	$this->dropTable('users');
     }
 
 }
